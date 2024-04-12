18832a38b00018ba1778a814e689647862a280c8
Andrei19941967 <sytenkovat@yandex.ru>
2024-04-12 15:52:15 +0300

    Dict

---
+++ Dict3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Dict3
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Dictionary<string, string> m = new Dictionary<string, string>()//первый словарь-ключ(имя птиомца)-значения это имя хозяина
+            {
+                {"Hatiko", "Parker"},
+                {"Fido", "John"},
+                {"Barry", "Parker"},
+                {"Rocky", "John"}
+            };
+            Dictionary<string, List<string>> v = new Dictionary<string, List<string>>();//новый словарь чтобы ключ это имя хозяина а значение это список питомца
+            foreach (string k in m.Keys)//цикл по всем ключам первого словаря
+            {
+                if(v.ContainsKey(m[k]))//если вторйо словарь новый сощдердит такое имя хозяина 
+                {
+                    v[m[k]].Add(k);//в список добовляем нового питомца
+                }
+                else//если такого хоязина еще не было в словаре
+                {
+                    List < string > b = new List<string>();//список кличек-питомцев
+                    b.Add(k);//добавили первого питомца
+                    v[m[k]] = b;//присвоили этот список по ключу
+                }
+            }
+
+            foreach (string c in v.Keys)//перебираем ключи из ноовго словаря
+            {
+                Console.Write(c + " ");//выводим ключ-тоесть имя хозяина
+                foreach (string h in v[c])//перебираем список питомцев
+                {
+                    Console.Write(h + " ");//выводим клички питомцев
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
